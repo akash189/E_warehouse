40a50f64421822629ec111c84b5eb0dff8dc5267
Akash S. Awhale <awhaleakash123@gmail.com>
2023-09-03 16:23:53 +0530

    commit

--- FronEnd/src/Components/Customers_List.jsx
+++ FronEnd/src/Components/Customers_List.jsx
@@ -1,43 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from "./Navbar";
 import AdminDashboard from './AdminDashboard';
 import AdminNav from './AdminNav';
+import axios from 'axios';
 
 
 function Customers_List() {
-  const customer = [
-    {
-        id :'100',
-        name: "Damodhar jadhao",
-        email: "Dj@gamil.com",
-        contact:"9923064941",
-        password:"abc@123",
-        city:"Pune"
-    },
-    {
-      id :'101',
-        name: "Abhishek Patil",
-        email: "Abhi@gamil.com",
-        contact:"9764338165",
-        password:"abhi@123",
-        city:"Sangali"
-    },
-    {
-      id :'102',
-        name: "Sagar Chavan",
-        email: "sagar@gamil.com",
-        contact:"8999969670",
-        password:"sagar@123",
-        city:"jalna"
+  // const customer = [
+  //   {
+  //       id :'100',
+  //       name: "Damodhar jadhao",
+  //       email: "Dj@gamil.com",
+  //       contact:"9923064941",
+  //       password:"abc@123",
+  //       city:"Pune"
+  //   },
+  //   {
+  //     id :'101',
+  //       name: "Abhishek Patil",
+  //       email: "Abhi@gamil.com",
+  //       contact:"9764338165",
+  //       password:"abhi@123",
+  //       city:"Sangali"
+  //   },
+  //   {
+  //     id :'102',
+  //       name: "Sagar Chavan",
+  //       email: "sagar@gamil.com",
+  //       contact:"8999969670",
+  //       password:"sagar@123",
+  //       city:"jalna"
+  //   }
+  // ];
+
+  const [customer, setCustomer] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8080/warehouse/customer/data").then(data => {
+      setCustomer(data.data)
     }
-  ];
+    ).catch(
+      (error) => { console.error(error) }
+    )
+  }, [])
 
 
   return (
     <>
-       <AdminNav/>
-       <h3 className="text-center my-3">All Customers</h3>
-            <hr />
+      <AdminNav />
+      <h3 className="text-center my-3">All Customers</h3>
+      <hr />
       <div className="table my-2 container">
         <table className="table table-dark table-hover">
           <thead>
